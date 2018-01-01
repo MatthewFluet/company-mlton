b4e659589728e90f11af4daebf87670a0f0cff5a
Matthew Fluet <matthew.fluet@gmail.com>
2018-01-01 09:25:46 -0500

    Update description

--- company-mlton.el
+++ company-mlton.el
@@ -28,9 +28,7 @@
 ;; It provides completion for Standard ML keywords and for Standard ML
 ;; (long) identifiers.  Candidate completion identifiers for the
 ;; latter are loaded from a basis file created by 'mlton' using
-;; '-show-basis <file>' or '(*#showBasis "<file>"*)'.  `company-mlton'
-;; ships with a default basis file that corresponds to MLton's default
-;; environment (implicitly used when compiling a ".sml" file).
+;; '-show-basis <file>' or '(*#showBasis "<file>"*)'.
 
 ;;; Code:
 
